5cfffa1f53906159b48ef2a605b76e9b79fe1919
Luís Antônio Martins dos Reis <luis.martins.reis@outlook.com>
2018-05-26 10:05:00 -0300

    alteracoes de classe

---
+++ src/main/java/com/handson/domain/Artist.java
@@ -0,0 +1,5 @@
+package com.handson.domain;
+
+public class Artist {
+
+}
--- src/main/java/com/handson/domain/Music.java
+++ src/main/java/com/handson/domain/Music.java
@@ -1,5 +1,27 @@
 package com.handson.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Music {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "music_id")
+	private String id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "artist_id")
+	private Artist artist;
 }
